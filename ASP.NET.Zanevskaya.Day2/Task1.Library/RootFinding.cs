f26c2cf80e7db11b5ef7ae4676c51d56a6a04e50
Ann Zanevskaya <ann.zanevskaya7@gmail.com>
2015-07-13 14:42:43 +0300

    Day2

---
+++ ASP.NET.Zanevskaya.Day2/Task1.Library/RootFinding.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Library
+{
+   public class RootFinding
+    {
+        public static double NewtonRoot(int number, int power, int guess, double e)
+        {
+            double x = 0;
+            double xprev = 0;
+            double df = 0;
+
+            x = guess;
+            while(true)
+            {
+                df = -(Math.Pow(x, power) - number) / (power * Math.Pow(x, power - 1));
+                if(Math.Abs(x - xprev) < e) break;
+                xprev = x;
+                x = x + df;  
+            }
+            return x;
+        }
+    }
+}
---
+++ ASP.NET.Zanevskaya.Day2/Task1/Root.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1.Library;
+
+namespace Task1
+{
+    class Root
+    {
+        static void Main()
+        {
+            double root;
+            int number; 
+            int power;
+            int guess;
+            const double e = 1e-8;
+
+            number = 278;
+            power = 4;
+            guess = 10;
+            root = RootFinding.NewtonRoot(number, power, guess, e);
+
+            Console.WriteLine("Newton Root {0}, System Root {1}", root, Math.Pow(number, 1.0 / power));
+        }
+    }
+}
